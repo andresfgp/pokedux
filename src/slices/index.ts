52ba6b8af8be9ade8d7e1f8943f970223a68b069
Andres Garcia <andres.fgp@hotmail.com>
2022-10-08 00:57:40 -0500

    middleware store

--- src/slices/index.ts
+++ src/slices/index.ts
@@ -1,22 +1,28 @@
 // slices/index.ts
-import { configureStore } from "@reduxjs/toolkit";
-import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
-import { dataReducer } from "./dataSlice";
-import { uiReducer } from "./uiSlice";
+import { configureStore } from '@reduxjs/toolkit';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import { dataReducer } from './dataSlice';
+import { uiReducer } from './uiSlice';
 
 const store = configureStore({
-    reducer: {
-        data: dataReducer,
-        ui : uiReducer
-    }
-})
+  reducer: {
+    data: dataReducer,
+    ui: uiReducer,
+  },
+  middleware: (getDefaultMiddleware) => [
+    ...getDefaultMiddleware({
+      immutableCheck: false,
+      serializableCheck: false,
+    }),
+  ],
+});
 
-type RootState = ReturnType<typeof store.getState>
+type RootState = ReturnType<typeof store.getState>;
 
-type Appdispatch = typeof store.dispatch
+type Appdispatch = typeof store.dispatch;
 
-export const useAppDispatch : ()=> Appdispatch = useDispatch;
+export const useAppDispatch: () => Appdispatch = useDispatch;
 
-export const useAppSelector : TypedUseSelectorHook<RootState> = useSelector;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
-export { store }
+export { store };
